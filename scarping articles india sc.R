e67d30505291638c8ddcd61c82f11e4fe4e13b69
CAROLA CORTI <barosa.carola@gmail.com>
2021-02-24 19:46:57 +0100

    changes

---
+++ scarping articles india sc.R
@@ -0,0 +1,21 @@
+#let's inspect the "urbanet" site -> to scrape the articles about india sc criticism 
+
+
+library(rvest)
+library(tidyverse)
+
+
+url <-"https://www.urbanet.info/tag/india/" 
+
+download.file(url, destfile = here::here ("mypage.html"))
+
+
+smart_c_debate <- read_html(here::here("mypage.html")) %>%
+  html_nodes(".fusion-post-content-wrapper div div :nth-child(1)") %>%
+  html_text()
+
+smart_c_debate
+
+
+
+
