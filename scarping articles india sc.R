50e221035b6636bcf62ed10f25b905aca17b2b25
CAROLA CORTI <barosa.carola@gmail.com>
2021-02-25 20:08:17 +0100

    scraping commit

--- scarping articles india sc.R
+++ scarping articles india sc.R
@@ -1,50 +1,36 @@
 #######################################################
 #######################################################
 
-# let's inspect the "urbanet" site -> to scrape the articles about india smart cities criticism 
-# url is this one -->  https://www.urbanet.info/tag/india/
-# intent to scrape the articles to lead text analysis in quanteda + sentiment analysis + dictionary analysis
-# + topic analysis to discuss how it is perceived 
+# inspect + scrape the "URBANET" site -> to scrape the articles about smart cities in India 
+# url -->  https://www.urbanet.info/tag/india/
+# intent to scrape the articles to run text analyses in quanteda + sentiment analysis + dictionary analysis
+# + topic analysis 
 
 #######################################################
 #######################################################
 
 
-# robots.txt already inspected: I am allowed to do it!! 
+# 1) Robots.txt already inspected: I am allowed to scrape! I have now to ask politely using rvest + httr   
 
+
+library(httr)
 library(rvest)
-library(tidyverse)
 
-url <-"https://www.urbanet.info/tag/india/" 
+url <-"https://www.urbanet.info/tag/india/"
+session <- html_session(url, 
+                        add_headers(version = version$version.string
+                        ))
+
+
+# 2) Download the URL
 
 download.file(url, destfile = here::here ("mypage.html"))
 
-smart_c_debate <- read_html(here::here("mypage.html")) %>%
+# 3) Insert the selected CSS path  
+
+sc_india_debate <- read_html(here::here("mypage.html")) %>%
   html_nodes("#posts-container :nth-child(1)") %>%
   html_text()
 
-smart_c_debate
 
-#EXTRACTING JUST THE CONTENT IN THE OREVIEW OF THE ARTICLES: OK 
-
-smart_c_debate_content <- read_html(here::here("mypage.html")) %>%
-  html_nodes (".fusion-post-content-container p") %>%
-  html_text()
- 
-smart_c_debate_content
-
-
-# CORPUS IN QUANTEDA 
-
-library(quanteda)
-mycorp <-corpus(smart_c_debate)
-
-
-
-myDfm <- dfm(mycorp , remove = stopwords("english"), tolower = TRUE,
-             remove_punct = TRUE, remove_numbers=TRUE, remove_symbols=TRUE)
-
-
-
-textplot_wordcloud(myDfm)
-
+sc_india_debate
